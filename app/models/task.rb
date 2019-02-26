88b293666385df827ef908e205ee1782c67f381c
tatsuya <ryuuma1005@gmail.com>
2019-02-26 09:16:42 +0900

    sort-change-can

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,7 @@ class Task < ApplicationRecord
 	scope :recent, -> { order(created_at: :desc) }
 
 	def self.ransackable_attributes(auth_object = nil)
-		%w[name create_at]
+		%w[name created_at]
 	end
 
 	def self.ransackable_associations(auth_object = nil)
